75f8259f99e0203babdac2a572e58af12f70e2c2
Sravan K J <kjsravanjyothis@hotmail.com>
2025-09-25 11:05:50 +0530

    changes in schema

--- internal/schema/user.go
+++ internal/schema/user.go
@@ -1,5 +1,7 @@
 package schema
 
-func user() {
-
+type user struct {
+	ID           int64  `bun:",pk,autoincriment"`
+	Username     string `bun:",notnull,unique"`
+	passwordhash string `bun:",notnull"`
 }
